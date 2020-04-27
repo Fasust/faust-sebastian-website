819f9bb209c71cfdb0e6360eb4f97068a21a7b54
Sebastian Faust <sebastian.faust@smail.th-koeln.de>
2020-04-27 15:27:27 +0200

    Fix bug: Navbar on mobile looks strange when on top of the page

--- js/responsive-nav-bar.js
+++ js/responsive-nav-bar.js
@@ -1,21 +1,27 @@
 $(window).scroll(function() {
     if ($(document).scrollTop() > 1) {
         $('.nav').addClass('affix');
+        $('.nav').removeClass('on-top');
+
     } else {
         $('.nav').removeClass('affix');
+        $('.nav').addClass('on-top');
     }
 });
 
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
+
+    if($('.nav').hasClass('on-top')){
+        $('.nav').toggleClass('affix')
+    }
+
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
 
 $('.navlinks li a').click(function () {
     $('.navTrigger').toggleClass('active');
-    console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 }); 
