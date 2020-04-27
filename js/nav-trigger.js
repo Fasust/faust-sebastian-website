425d80a5873674fc728881693123270b863dbe76
Sebastian Faust <sebastian.faust@smail.th-koeln.de>
2020-04-27 14:50:41 +0200

    Close Burger menu after navigation

--- js/nav-trigger.js
+++ js/nav-trigger.js
@@ -3,5 +3,12 @@ $('.navTrigger').click(function () {
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
-
 });
+
+$('.navlinks li a').click(function () {
+    $('.navTrigger').toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
+
