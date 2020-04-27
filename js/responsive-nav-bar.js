036aad8804d881b70c16205e67ef526191b2a318
Sebastian Faust <sebastian.faust@smail.th-koeln.de>
2020-04-27 14:53:29 +0200

    Refactor nav bar js

--- js/responsive-nav-bar.js
+++ js/responsive-nav-bar.js
@@ -1,3 +1,11 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 1) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
